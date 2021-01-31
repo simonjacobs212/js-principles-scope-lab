70116936b8701c10ddf1f8de43e6d2a13fd96aee
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-31 14:53:46 -0500

    commit tree ac0914817704e9d9709c520311fb2cf029054ce1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+const leastFavoriteCustomer = 'something'
+
+var bestCustomer;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
